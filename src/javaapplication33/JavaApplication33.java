57eae47dc9a7e46f72265fa0dad96c5b1e80fb82
joewin-eng <115566698+joewin-eng@users.noreply.github.com>
2022-11-08 19:19:23 +0800

    First Commit

---
+++ src/javaapplication33/JavaApplication33.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication33;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Joewi
+ */
+public class JavaApplication33 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        int angka;
+        System.out.println("Masukan angka = ");
+        angka = input.nextInt();
+        if (angka == 0)
+        {    
+            System.out.println("off");
+        }
+        else
+        {
+            System.out.println("On");
+        }
+       
+        
+        }
+       
+            
+        }
+    
+    
+

8e8704e4da5b7dcddad6342b45fd1ca59e5ffdec
joewin-eng <115566698+joewin-eng@users.noreply.github.com>
2022-11-08 22:05:22 +0800

    First Commit

---
+++ if else/src/pkgif/pkgelse/IfElse.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkgelse;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Joewi
+ */
+public class IfElse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String tanya;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Apakah anda lapar?(Ya atau Tidak) : " );
+        tanya = input.next();
+        if ("Ya".equalsIgnoreCase(tanya))
+        {
+            System.out.println("Makan");
+            
+        }
+        else
+        {
+            System.out.println("Tidak Makan");
+          
+        }
+                
+    }
+    
+}

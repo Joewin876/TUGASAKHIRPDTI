9403f4b1af6beb7162daece8fbfaf4207b6f5c1b
joewin-eng <115566698+joewin-eng@users.noreply.github.com>
2022-11-22 11:09:21 +0800

    First Commit

---
+++ JavaApplication59/src/javaapplication59/JavaApplication59.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication59;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Joewi
+ */
+public class JavaApplication59 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner(System.in);
+        String daftarnama;
+        System.out.println("Nama Siswa 1 = ");
+        daftarnama=input.next();
+        System.out.println("Nama Siswa 2 = ");
+        daftarnama=input.next();
+        System.out.println("Nama Siswa 3 = ");
+        daftarnama=input.next();
+        System.out.println("Nama Siswa 4 = ");
+        daftarnama=input.next();
+        
+       
+        
+    }
+    
+}
